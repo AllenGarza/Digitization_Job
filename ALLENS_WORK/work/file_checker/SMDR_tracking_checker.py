e7ab98d2426659b559e1ed787d1d72ab07e70988
AllenGarza <37097946+AllenGarza@users.noreply.github.com>
2021-01-18 18:30:12 -0600

    Add files via upload

---
+++ ALLENS_WORK/work/file_checker/SMDR_tracking_checker.py
@@ -0,0 +1,36 @@
+"""SMDR_tracking_checker
+1/18/2021
+@Author Allen Garza
+
+This program will compare an SMDR tracking sheet with its
+corresponding 'spotted images' and 'tospot images' folders
+to ensure the all images are accounted for.
+
+"""
+
+import os
+import sys
+import checker
+import pandas as pd
+
+
+def main():
+    if len(sys.argv) != 3:
+        print('Usage: SMDR_tracking_checker [tracking_dir] [spotted_imgs_dir] [tospot_imgs_dir] \n')
+        print('Outputs ***_OUT_.xlsx to wherever your tracking_dir is stored.')
+
+    # Testing #
+    # tracking_dir = r'C:\Users\allen\PycharmProjects\work\file_checker\checker_env\SMDR_1961_Capture-and-Processing'
+    # spotted_imgs_dir = r'C:\Users\allen\PycharmProjects\work\file_checker\checker_env\batch1\toqc'
+    # tospot_imgs_dir = r'C:\Users\allen\PycharmProjects\work\file_checker\checker_env\batch1'
+    # Testing #
+
+    check = checker(sys.argv[1], sys.argv[2], sys.argv[3])
+    # check = checker.Checker(tracking_dir, spotted_imgs_dir, tospot_imgs_dir)
+    check.save_df()
+
+    quit()
+
+
+if __name__ == "__main__":
+    main()
--- ALLENS_WORK/work/file_checker/checker.py
+++ ALLENS_WORK/work/file_checker/checker.py
@@ -1,20 +1,49 @@
+"""SMDR_tracking_checker
+1/18/2020
+@Author Allen Garza
+
+This is meant to be used in conjunction with SMDR_tracking_checker.
+"""
+
 import re
 import os
-import sys
-import matplotlib.pyplot as plt
 import pandas as pd
-import cv2
 import openpyxl
-import numpy as np
 from collections import defaultdict
+import copy
 
 
 class Checker:
+    """Checker class, handles and processes data, saves final .xlsx
+
+    The 'Checker' class is what holds the methods for loading the excel sheet, as well as images
+    in a directory and parsing the data. We use the 'num_spotted' and 'num_tospot' columns and create our own
+    columns listed as 'num_spotted_in_folder' and 'num_tospot_in_folder'. They will list how many of the images there are
+    in each corresponding folder. Returns a saved ***-OUT-.xlsx wherever the directory of the original tracking sheet
+    is.
+
+    Args:
+        dir_of_tracking (str): directory of where tracking sheet is.
+        dir_of_spotted_imgs(str): directory of where spotted images are saved, typically '../toqc/ .
+        dir_of_tospot_imgs(str): directory of where tospot images are saved.
+
+    Attributes:
+        dir_of_tracking (str): directory of where tracking sheet is.
+        dir_of_spotted_imgs(str): directory of where spotted images are saved, typically '../toqc/ .
+        dir_of_tospot_imgs(str): directory of where tospot images are saved.
+        tracking_df(pd.Dataframe): dataframe containing our info.
+        file_name(str): the base name of the file of the sheet.
+        counter(float/int): a counter used for various operations.
+        spotted_or_tospot(bool): a selector of the order of operations.
+
+    """
 
     def __init__(self, dir_of_tracking, dir_of_spotted_imgs, dir_of_tospot_imgs):
         self.tracking_df = None
-        self.spotted_imgs_df = None
-        self.tospot_imgs_df = None
+        self.file_name = os.path.basename(dir_of_tracking)
+
+        self.counter = None
+        self.spotted_or_tospot = False # false implies spotted, refer to load_spotted_images_info() and df_updater()
 
         self.dir_of_tracking = dir_of_tracking
         self.dir_of_spotted_imgs = dir_of_spotted_imgs
@@ -23,20 +52,34 @@ class Checker:
         Checker.load_tracking(self)  # loads database (excel tracking sheet)
         Checker.load_spotted_imgs_info(self)  # creates df for spotted imgs info
         Checker.load_tospot_imgs_info(self)  # creates df for tospot imgs info
-        Checker.compare_dfs(self)
 
-        print(dir_of_tracking.head())
+    def load_tracking(self): # -> dataframe
+        """Loads pd.Dataframe from the tracking .xlsx sheet.
 
-    def load_tracking(self):
+        We filter columns and rename what's left.
+        A cap of 500 rows is set to improve load times, change if necessary.
+
+        :return:
+        """
+
         dir_of_tracking_xlsx = self.dir_of_tracking + '.xlsx'
-        df = pd.read_excel(dir_of_tracking_xlsx)
-        df_refined = df[['Envelope #', '# Spotted', 'ToSpot']]
+        df = pd.read_excel(dir_of_tracking_xlsx, nrows= 500)
+        df_refined = df[['Envelope Title', 'Envelope #', '# Spotted', 'ToSpot']]
         df_refined = df_refined.rename(columns={"Envelope #": "envelope_num", "# Spotted": "num_spotted"})
         self.tracking_df = df_refined
+        print('tracking loaded \n')
 
-    def load_spotted_imgs_info(self):
-        spotted_imgs_df = self.tracking_df
-        spotted_imgs_df['num_spotted_in_folder'] = float(0)
+    def load_spotted_imgs_info(self): # -> dataframe
+        """Counts number of images are in a folder.
+
+        A regex pattern is used, to ensure the files correspond to the correct
+        envelope numbers, and are in fact images.
+
+        :returns dataframe
+        """
+
+        self.spotted_or_tospot = False
+        self.tracking_df['num_spotted_in_folder'] = None
 
         # SMDR_1961-Envelope#_XXX ## NAMING TEMPLATE ##
         pattern = re.compile("^([A-Z]+.\d+).(\d+)(.)(\d+)")  # we only care about groups 2 and 4
@@ -45,78 +88,123 @@ class Checker:
         # do character match using regex, find 2nd group of characters and 4th, containing
         # "Envelope#" and "XXX", then we parse 'Envelope#' and 'XXX' as float64, add to new dataframe
 
-        initial_env_num = self.initial_file(pattern) # find inital file at top of list of imgs in folder using regex pattern
+        initial_env_num = self.initial_file(pattern) # find initial file at top of list of imgs in folder using regex pattern
 
-        self.spotted_imgs_df = self.df_updater(spotted_imgs_df, initial_env_num, pattern)
+        self.df_updater(initial_env_num, pattern)
 
+        print('spotted imgs loaded \n')
 
-    def load_tospot_imgs_info(self):
-        tospot_imgs_df = pd.DataFrame(columns=['envelope_num', 'ToSpot_in_folder'], dtype=float)
+    def load_tospot_imgs_info(self): # -> dataframe
+        """Counts number of images are in a folder.
+
+        A regex pattern is used,to ensure the files correspond to the correct envelope numbers,
+        and are in fact images.
+
+        :returns dataframe
+        """
+
+        self.spotted_or_tospot = True
+        self.tracking_df['num_ToSpot_in_folder'] = None
 
         # SMDR_1961-Envelope#_XXX ## NAMING TEMPLATE ##
         pattern = re.compile("^([A-Z]+.\d+).(\d+)(.)(\d+)")  # only care about groups 2 and 4
 
-        envelope_num_master = None
-        for f in os.listdir(self.dir_of_tospot_imgs):
-            if f[-4:] == ".tif" or ".jpg":
-                imgstr = str(f)
-                m = pattern.match(imgstr)
-                envelope_num = float(m.group(2))
-                img_num = float(m.group(4))
-                if spotted_imgs_df[spotted_imgs_df.iloc[:, ]]
-                spotted_imgs_df = tospot_imgs_df.append({'envelope_num': envelope_num, 'ToSpot_in_folder': img_num}, ignore_index=True)
+        initial_env_num = self.initial_file(pattern)
 
-        self.tospot_imgs_df = tospot_imgs_df
+        self.df_updater(initial_env_num, pattern)
 
-    def compare_dfs(self): #    RE-EVALUATE #####################
-        # We now have 3 different dfs.
-        # We will merge and compare them to find any potential discrepancies
+        print('to spot imgs loaded \n')
 
-        tracking_df = self.tracking_df
-        spotted_df = self.spotted_imgs_df
-        tospot_imgs_df = self.tospot_imgs_df
+    def save_df(self): # -> .xlsx
+        """saves final dataframe.
 
-        tracking_df
+        Changes working directory to where the original tracking sheet is saved, saves new dataframe as
+        ***-OUT-.xlsx file in same directory.
+        :return:
+        """
 
-        pass
-
+        d = len(os.path.basename(self.dir_of_tracking))
+        save_dir = self.dir_of_tracking[:-d]
+        os.chdir(save_dir)
+        output_name = self.file_name[:-4] + "-OUT-.xlsx"
+        self.tracking_df.to_excel(output_name)
+        print('SAVED')
 
     # allows us to find the top of the list of imgs, letting us know what envelope number our list begins with
     def initial_file(self, pattern):
+        """finds the first file.
+
+        We do this as, know the files will follow one another from top to bottom. We obtain the top images name and env_num,
+        knowing the rest will follow.
+
+        :param pattern: regex pattern obtained from template naming of names.
+        :return:  initial_env_number
+        """
+
         file_list = os.listdir(self.dir_of_spotted_imgs)    # list containing list of files
-        f = -1      # counter
+        self.counter = 0
 
         while True:
-            f = f + 1
-            curr_env_num = str(file_list[f])
-            if f[:-4] != '.tif':    # if f is not the first image at the top of the list of imgs in dir we skip that file and restart loop
+            curr_env_num = str(file_list[self.counter]) # 'XXXXXXXX.tif'
+            if curr_env_num[-4:] != '.tif':    # if f is not the first image at the top of the list of imgs in dir we skip that file and restart loop
+                self.counter = self.counter + 1
                 continue
             m = pattern.match(curr_env_num)
             initial_env_num = float(m.group(2))  # make an "initial counter"
             return initial_env_num
             break
 
-    def df_updater(self, df, curr_env_num, pattern):
+    def df_updater(self, curr_env_num, pattern):
+        """Counts images, ensures they correspond to a specific entry, saves this information into dataframe.
+        Does this sequentially, can change this to do divide and conquer in the future sometime maybe.
 
-        for f in os.listdir(self.dir_of_spotted_imgs):
-            if f[:-4] != '.tif':    # a quick check to ensure it is the correct format '.tif'
-                continue
+        :param curr_env_num: starting envelope number.
+        :param pattern: pattern of naming template.
+        :return: dataframe
+        """
 
-            
-            imgstr = str(f)
-            m = pattern.match(imgstr)
-            envelope_num = float(m.group(2))
-            # img_num = float(m.group(4))
+        # recall, self.spotted_or_tospot is False
+        self.counter = 0.0
 
-            if envelope_num == curr_env_num:
-
-
-            else
+        if not self.spotted_or_tospot:
+            for f in os.listdir(self.dir_of_spotted_imgs):
+                if f[-4:] != '.tif':    # a quick check to ensure it is the correct format '.tif'
+                    continue
 
 
-            specific_row = df.loc[df.envelope_num == envelope_num]
+                imgstr = str(f)
+                m = pattern.match(imgstr)
+                envelope_num = float(m.group(2))
+                # img_num = float(m.group(4))
 
-            return spotted_imgs_df
+                if envelope_num == curr_env_num:
+                    self.counter = self.counter + 1.0
+                    num = copy.deepcopy(self.counter)
+                    self.tracking_df.loc[self.tracking_df['envelope_num'] == curr_env_num, ['num_spotted_in_folder']] = num
+                else:
+                    num = copy.deepcopy(self.counter)
+                    self.tracking_df.loc[self.tracking_df['envelope_num'] == curr_env_num, ['num_spotted_in_folder']] = num
+                    self.counter = 0.0
+                    continue
+
+        else: # implies we're working on ToSpot folder
+            for f in os.listdir(self.dir_of_tospot_imgs):
+                if f[-4:] != '.tif':    # a quick check to ensure it is the correct format '.tif'
+                    continue
+
+
+                imgstr = str(f)
+                m = pattern.match(imgstr)
+                envelope_num = float(m.group(2))
+                # img_num = float(m.group(4))
+
+                if envelope_num == curr_env_num:
+                    self.counter = self.counter + 1.0
+                else:
+                    num = copy.deepcopy(self.counter)
+                    self.tracking_df.loc[self.tracking_df['envelope_num'] == curr_env_num, ['num_ToSpot_in_folder']] = num
+                    self.counter = 0.0
+                    continue
 
 
 
