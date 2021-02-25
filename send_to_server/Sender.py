1ab2a12a423f075438ada552702459da90a8b995
AllenGarza <37097946+AllenGarza@users.noreply.github.com>
2021-02-24 20:59:11 -0600

    Add files via upload

--- send_to_server/Sender.py
+++ send_to_server/Sender.py
@@ -1,46 +1,26 @@
 import os
-import sys
-import re
 from multipledispatch import dispatch
-import image
-from collections import defaultdict
-from shutil import copyfile  # we'll use copy2
+from shutil import copyfile
 
 
 class Sender:
-    '''
-    during construction, our sender checks and loads files
-
-    finds the pattern (list of patterns can be added to, consider creating
-    an abstract class to make this easier(?) to modify).
-    '''
-
     def __init__(self, *args):
-
-        self.input_dir = None
-        self.output_dir = None
-
-        self.counter = 0
         self.file_size = 0
 
-        self.envelopes = []  # list containing titles of envelopes, will use as keys later in file_divider
         self.file_paths = []
-        self.file_divider = {}  # key should match value of image.output_dir, value is actual output
-        self.file_mapper = {}
+        self.envelopes_in = []
+        self.envelopes_out = []
+        self.envelopes_mapped_to_extension = []
+        self.envelope_mapped_to_paths = dict()
+        self.file_divider_input = dict()
+        self.file_divider_output = dict()
         self.files = None
 
         self.dir_setter(*args)
-        self.file_divider_setter()  # sets up our output_dir dictionary
-        self.order_files()  # assigns the imgs in self.files_jpg and *_tif to the respective dirs.
-
-    '''Categorize files into two vectors, one of tifs, other of jpgs, obtain file size of each vector as well
-    in tif_size and jpg_size.
-    
-    '''
+        self.check_and_load_files()
 
     @dispatch(str, str)
     def dir_setter(self, input_dir, output_dir):  # the case where only 1 input and 1 output are given.
-
         if not os.path.isdir(input_dir):
             print("ERROR: Input Directory is not a true directory.")
             exit()
@@ -49,45 +29,56 @@ class Sender:
         else:
             dirs_containing_imgs = os.listdir(input_dir)  # a list containing all of our 'envelopes'
             dirs_to_contain_imgs = os.listdir(output_dir)  # output list containing all of our 'envelopes'
-            for dir in dirs_containing_imgs:  # each dir containing images
+
+            for dir in dirs_containing_imgs:  # each dir containing extensions dirs
                 envelope = str(os.path.basename(dir))  # get basename of dir (envelope)
-                self.envelopes.append(envelope)  # append to our list of envelopes
-                self.file_divider[envelope] = []  # use key and associate an empty list
+                self.file_divider_input[envelope] = set()
+                self.envelopes_in.append(envelope)  # append to our list of envelopes, will use them as keys
                 dir_path = os.path.join(input_dir, dir)  # get path of the envelope
-                file_extensions = os.listdir(
-                    dir_path)  # get names directories that show a separation by file type (tif, jpg) within envelope
-                for file_extension in file_extensions:  # for each directory that separates imgs by file type
-                    self.file_divider[envelope].append(file_extension)  # add file types within each envelope to a
-                    # list and associate list with envelopes
-                    path_of_files = os.path.join(dir_path, file_extension)
-                    self.file_paths = path_of_files
-                    self.file_mapper[path_of_files] = None
-                    self.check_and_load_files(path_of_files)
+                file_extensions = os.listdir(dir_path)  # separation by file type (tif, jpg) within envelope
+                for file_extension in file_extensions:  # for each directory that separates images by file type
+                    file_extension_path = os.path.join(dir_path, file_extension)
+                    self.file_divider_input[envelope].add(file_extension_path) # envelope maps to path of files by extension
 
-            file_type_ex = self.file_divider[self.envelopes[0]]
+            for dir in dirs_to_contain_imgs:
+                envelope = str(os.path.basename(dir))
+                self.file_divider_output[envelope] = set()
+                self.envelopes_out.append(envelope)
+                envelope_output_dir = os.path.join(output_dir, dir)
+                file_extensions = os.listdir(envelope_output_dir)
+                for file_extension in file_extensions:
+                    out_file_extension_path = os.path.join(envelope_output_dir, file_extension)
+                    self.file_divider_output[envelope].add(out_file_extension_path)
 
-            print("here are the directories . . . .\n")
-            print('INPUT dirs: ', dirs_to_contain_imgs, '\n')
-            print('OUTPUT dirs: ', dirs_to_contain_imgs, '\n')
-            print('Files are separated by:', file_type_ex)
+            print("here are our files by envelope . . . .\n")
+            print('INPUT: ', dirs_to_contain_imgs, '\n')
+            print('OUTPUT: ', dirs_to_contain_imgs, '\n')
             print('Is this correct?')
+            # place prompt here, then begin transfer, check_and_load_files() called in __init__
 
-    @property
-    def check_and_load_files(self, path_of_files):
-        files = os.listdir(path_of_files)
-        size_of_directory = 0
-        for file in files:
-            file_path = os.path.join(path_of_files, file)
-            size_of_directory = size_of_directory + os.stat(file_path)  # in bytes
+    # the following assums all directories are created and matched.
+    # should probably consider the case where directories aren't already established
+    def check_and_load_files(self):
+        for envelope in self.envelopes_in:
+            assert envelope in self.envelopes_out
+            file_extension_paths_input = list(self.file_divider_input[envelope])
+            file_extension_paths_output = list(self.file_divider_output[envelope])
 
-        size_of_directory = size_of_directory * 10 ^ (-6)  # in MB
-        # we now know the size of files in each subdirectory of the file types.
-        # lets associate the size with the address.
+            num_of_extensions = len(self.file_divider_input[envelope])
+            assert num_of_extensions == len(self.file_divider_output[envelope])
 
-        self.file_mapper[
-            path_of_files] = size_of_directory  # mapping the address of each group of files to values of their size.
+            for x in range(num_of_extensions):
+                file_path_in = file_extension_paths_input.pop()
+                file_path_out = file_extension_paths_output.pop()
+                files = os.listdir(file_path_in)
+                for file in files:
+                    file_path_in_specific = os.path.join(file_path_in, file)
+                    file_path_out_specific = os.path.join(file_path_out, file)
+                    copyfile(file_path_in_specific, file_path_out_specific)
 
+ # not yet implimented ########
     def prompt_copy(self):
+        total = 0
         for dir_containing_imgs in self.file_paths:
             file_ext = os.path.basename(dir_containing_imgs)  # (tif, jpg, tospot, etc.)
 
@@ -103,5 +94,4 @@ class Sender:
             elif reply[:1] == 'n':
                 return False
 
-    def copy_files(self):
-        copy = self.prompt_copy()
+
